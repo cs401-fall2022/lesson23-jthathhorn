51929961ee978752ca0ab03b2b411a78aaa897ff
jthathhhorn <jasonhathhorn@u.boisestate.edu>
2022-11-18 15:51:16 -0700

    start 'update' funciton

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.get('/', function (req, res, next) {
         (err, rows) => {
           if (rows.length === 1) {
             console.log("Table exists!");
-            db.all(` select blog_id, blog_txt from blog`, (err, rows) => {
+            db.all(` SELECT blog_id, blog_txt FROM blog`, (err, rows) => {
               console.log("returning " + rows.length + " records");
               res.render('index', { title: 'Blog', data: rows });
             });
@@ -26,10 +26,10 @@ router.get('/', function (req, res, next) {
                      blog_id INTEGER PRIMARY KEY AUTOINCREMENT,
                      blog_txt text NOT NULL);
                       INSERT into blog (blog_txt)
-                      values ('This is a great blog'),
+                      VALUES ('This is a great blog'),
                              ('Oh my goodness blogging is fun');`,
               () => {
-                db.all(` select blog_id, blog_txt from blog`, (err, rows) => {
+                db.all(` SELECT blog_id, blog_txt FROM blog`, (err, rows) => {
                   res.render('index', { title: 'Blog', data: rows });
                 });
               });
@@ -48,9 +48,6 @@ router.post('/add', (req, res, next) => {
         exit(1);
       }
       console.log("inserting " + req.body.blog);
-      //NOTE: This is dangerous! you need to sanitize input from the user
-      //db.exec(`insert into blog ( blog_txt) values ('${req.body.blog}');`)
-
       // sanitized statement
       db.run('INSERT into blog (blog_txt) VALUES (?);', req.body.blog);
       db.close();
@@ -60,8 +57,9 @@ router.post('/add', (req, res, next) => {
   );
 })
 
-router.post('/delete', (req, res, next) => {
-  console.log("deleting stuff without checking if it is valid! SEND IT!");
+// edit??? return err statement if post DNE?
+router.post('/update', (req, res, next) => {
+  console.log("editing blog post...");
   var db = new sqlite3.Database('mydb.sqlite3',
     sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
     (err) => {
@@ -70,9 +68,6 @@ router.post('/delete', (req, res, next) => {
         exit(1);
       }
       console.log("Deleting blog post # " + req.body.blog);
-      //NOTE: This is dangerous! you need to sanitize input from the user
-      //db.exec(`delete from blog where blog_id='${req.body.blog}';`);
-      
       // sanitized statement
       db.run('DELETE from blog WHERE blog_id=(?)', req.body.blog)
       db.close();
@@ -80,5 +75,33 @@ router.post('/delete', (req, res, next) => {
     }
   );
 })
+
+
+router.post('/delete', (req, res, next) => {
+
+// need to validate data before delete?/handle err?
+  console.log("deleting stuff without checking if it is valid! SEND IT!");
+  
+  var db = new sqlite3.Database('mydb.sqlite3',
+    sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+    (err) => {
+      if (err) {
+        console.log("Getting error " + err);
+        exit(1);
+      }
+      console.log("Deleting blog post # " + req.body.blog);
+      // sanitized statement
+      db.run('DELETE from blog WHERE blog_id=(?)', req.body.blog)
+      db.close();
+      res.redirect('/');
+    }
+  );
+})
+
+
+
+
+
+
 
 module.exports = router;
